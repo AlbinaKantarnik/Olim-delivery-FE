cea51bceb3941ba5b3b24c0242f466f612c33e41
Albina Kantarnik <karitskaja@mail.ru>
2024-03-12 00:40:57 +0200

    перед pull 1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Olim delivery
+https://olim-delivery.netlify.app/
 
 ## Home page
+
+
+*upstream
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
